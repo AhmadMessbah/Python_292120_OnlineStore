1e4e0e5ac361257c08a6719c795dbb8c7c5d7cce
Negin <neginsz1234@gmail.com>
2024-01-26 18:25:27 +0330

    SqlAlchemy Added - Negin saatsaz

---
+++ model/entity/base.py
@@ -0,0 +1,11 @@
+import json
+
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    def json(self):
+        return str({c.name: getattr(self, c.name) for c in self.__table__.columns})
+
+    def __repr__(self):
+        return str({c.name: getattr(self, c.name) for c in self.__table__.columns})
