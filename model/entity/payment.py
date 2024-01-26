60d149913969b72ad68ee5b45007b0117c255956
Negin <neginsz1234@gmail.com>
2024-01-26 16:32:29 +0330

    Payment/Stuff Updated - Negin saatsaz

---
+++ model/entity/payment.py
@@ -0,0 +1,7 @@
+class Payment:
+    def __init__(self, amount, payment_type, payment_date_time):
+        self.id = None
+        self.amount = amount
+        self.payment_type = payment_type
+        self.payment_date_time = payment_date_time
+
---
+++ model/entity/stuff.py
@@ -0,0 +1,7 @@
+class stuff:
+    def __init__(self, name, brand, model, buy_price, description):
+        self.id = None
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.description = description
