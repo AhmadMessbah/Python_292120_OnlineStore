0d8aeeee547395562d04695fb66c5e0cda3f9c51
Negin Saatsaz <neginsz1234@gmail.com>
2024-02-03 23:14:10 +0330

    update - Negin Saatsaz

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,27 +1,58 @@
+import mysql.connector
+
+from model.entity import user
+
+
 class UserController:
+    def connect(self):
+        self.connection = mysql.connector.connect(user='root', password='root123', database='mft')
+        self.cursor = self.connection.cursor()
+
+    def disconnect(self):
+        self.connection.close()
+        self.cursor.close()
+
+
     def save(self,  name, family, username, password, role, status=True):
-        pass
+        self.connect()
+        self.cursor.execute(operation:"INSERT INTO User_tbl (name, family, username, password, role, status) values (%s, %s, %s, %s, %s)",
+                            params[user.name , user.family , user.username , user.password , user.role , user.status])
+        self.connection.commit()
+        self.disconnect()
 
 
     def edit(self,  id, name, family, username, password, role, status):
-        pass
+        self.connect()
+        self.cursor.execute(operation:"UPDATE User_tbl set name = %s, family = %s, username = %s, password = %s , role = %s, status = %s where id = %s",
+        params:[user.name , user.family , user.username, user.password , user.role , user.id])
+        self.disconnect()
 
 
     def remove(self, id):
-        pass
+        self.connect()
+        self.cursor.execute(operation :"DELETE FROM User_tbl where id = %s" ,
+        params :[user.id])
 
 
     def find_all(self):
-        pass
-
-
-    def find_by_id(self, id):
-        pass
-
+        self.connect()
+        self.cursor.execute("select * from Users")
+        user = self.cursor.fetchall()
+        self.disconnect()
+        return user
 
     def find_by_username(self, username):
-        pass
+        self.connect()
+        self.cursor.execute(operation:"SELECT * from user_tbl where username = %s", params:[username])
+        user = self.cursor.fetchone()
+        self.disconnect()
+        return user
 
 
     def find_by_username_and_password(self, username, password):
-        pass
+        self.connect():
+        self.cursor.execute(operation:"SELECT * from user_tbl where username = %s AND PASSWORD =%s",
+        params:[user.username , user.password])
+        user = self.cursor.fetchone()
+        self.disconnect()
+        return user
