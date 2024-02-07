a66fc704c3a40c65f840e18d7827a5b1fca0c2c6
nnggin <154870582+nnggin@users.noreply.github.com>
2024-02-07 12:11:12 +0330

    Update payment.py

--- model/entity/payment.py
+++ model/entity/payment.py
@@ -4,14 +4,16 @@ from model.entity.base import Base
 class Payment(Base):
     __tablename__ = "payment_tbl"
     id = Column(Integer, primary_key=True)
+    user_id = column(Integer,foreingKey("user.id")
     name = Column(String(30))
     model = Column(String(30))
     buy_price = Column(Integer)
     description = Column(String(100))
 
 
-    def __init__(self,name,model,buy_price,description):
+    def __init__(self,name,,user_id,model,buy_price,description):
         self.id = None
+        self.user_id = user_id
         self.name = name
         self.model = model
         self.buy_price = buy_price
