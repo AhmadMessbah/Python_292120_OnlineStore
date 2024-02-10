33d0b181525333bb6452074f6e3014de38ec04aa
nnggin <154870582+nnggin@users.noreply.github.com>
2024-02-10 12:57:09 +0330

    Update storage_da.py

--- model/da/storage_da.py
+++ model/da/storage_da.py
@@ -4,12 +4,23 @@ from model.entity.storage import Storage
 
 class StorageDa(DatabaseManager):
 
+     def find_by_id(self, id):
+        self.make_engine()
+        result = self.session.query(Storage).filter(Storage.id == id)
+        return result
+
+
+    
     def find_by_stuff(self, stuff):
         self.make_engine()
-        entity = self.session.query(Storage).filter(Storage.stuff == stuff).all()
-        return entity.first()
+        result = self.session.query(Storage).filter(Storage.stuff == stuff)
+        return result
 
     def find_by_count(self, count):
         self.make_engine()
-        entity = self.session.query(Storage).filter(Storage.count == count).all()
-        return entity.first()
+        result = self.session.query(Storage).filter(Storage.count == count)
+        return result
+
+
+    def find_all(self, **kwargs):
+        pass
