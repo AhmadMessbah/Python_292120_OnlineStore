ccc05aec2061345094145b88dd823a77252d5237
nnggin <154870582+nnggin@users.noreply.github.com>
2024-02-10 12:47:37 +0330

    Update user_da.py

--- model/da/user_da.py
+++ model/da/user_da.py
@@ -7,22 +7,27 @@ class UserDa(DatabaseManager):
     def find_by_username(self, username):
         self.make_engine()
         entity = self.session.query(User).filter(User.username == username).all()
-        return entity.first()
+        return result
 
     def find_by_username_and_password(self, username, password):
         self.make_engine()
-        entity = self.session.query(User).filter( User.username == username, User.password == password).all()
-        return entity.first()
+        entity = self.session.query(User).filter( User.username == username, User.password == password)
+        return result
 
     def find_by_name_and_family(self, name, family):
         self.make_engine()
-        entity = self.session.query(User).filter( User.name == name, User.family == family).all()
-        return entity.first()
+        entity = self.session.query(User).filter( User.name == name, User.family == family)
+        return result)
 
     def find_by_phone(self, phone):
         self.make_engine()
-        entity = self.session.query(User).filter(User.phone == phone).all()
-        return entity.first()
+        entity = self.session.query(User).filter(User.phone == phone)
+        return result
+
+    def find_id(self, id):
+        self.make_engine()
+        result = self.session.query(User).filter(User.id == id)
+        return result
 
     def find_all(self, **kwargs):
         pass
