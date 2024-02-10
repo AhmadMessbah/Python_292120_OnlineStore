dabb379104cef9454b68d54a7b209c9b3f29635e
nnggin <154870582+nnggin@users.noreply.github.com>
2024-02-10 12:44:56 +0330

    Update user_da.py

--- model/da/user_da.py
+++ model/da/user_da.py
@@ -13,3 +13,17 @@ class UserDa(DatabaseManager):
         self.make_engine()
         entity = self.session.query(User).filter( User.username == username, User.password == password).all()
         return entity.first()
+
+    def find_by_name_and_family(self, name, family):
+        self.make_engine()
+        entity = self.session.query(User).filter( User.name == name, User.family == family).all()
+        return entity.first()
+
+    def find_by_phone(self, phone):
+        self.make_engine()
+        entity = self.session.query(User).filter(User.phone == phone).all()
+        return entity.first()
+
+    def find_all(self, **kwargs):
+        pass
+    
